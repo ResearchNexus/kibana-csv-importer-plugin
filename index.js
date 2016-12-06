a2833715467f3f17689156286b03aaf6026a22c1
Miha Eržen <miha.erzen1@gmail.com>
2016-12-06 09:22:25 +0100

    Ok... proof of concept landed

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 import exampleRoute from './server/routes/example';
+import esRoutes from './server/routes/es';
 
 export default function (kibana) {
-  return new kibana.Plugin({
-    require: ['elasticsearch'],
+    return new kibana.Plugin({
+        require: ['elasticsearch'],
 
-    uiExports: {
-      
-      app: {
-        title: 'Kibana Csv Importer Plugin',
-        description: 'An awesome Kibana plugin',
-        main: 'plugins/kibana_csv_importer_plugin/app'
-      },
-      
-      
-      hacks: [
-        'plugins/kibana_csv_importer_plugin/hack'
-      ]
-      
-    },
+        uiExports: {
 
-    config(Joi) {
-      return Joi.object({
-        enabled: Joi.boolean().default(true),
-      }).default();
-    },
+            app: {
+                title: 'Kibana Csv Importer Plugin',
+                description: 'An awesome Kibana plugin',
+                main: 'plugins/kibana_csv_importer_plugin/app'
+            },
 
-    
-    init(server, options) {
-      // Add server routes and initalize the plugin here
-      exampleRoute(server);
-    }
-    
 
-  });
+            hacks: [
+                'plugins/kibana_csv_importer_plugin/hack'
+            ]
+
+        },
+
+        config(Joi) {
+            return Joi.object({
+                enabled: Joi.boolean().default(true),
+            }).default();
+        },
+
+
+        init(server, options) {
+            // Add server routes and initalize the plugin here
+            exampleRoute(server);
+            esRoutes(server);
+        }
+    });
 };
--- public/app.js
+++ public/app.js
@@ -1,3 +1,7 @@
+import 'ng-file-upload';
+
+import _ from 'lodash';
+import Papa from 'papaparse';
 import moment from 'moment';
 import chrome from 'ui/chrome';
 import uiModules from 'ui/modules';
@@ -7,29 +11,120 @@ import 'ui/autoload/styles';
 import './less/main.less';
 import template from './templates/index.html';
 
+const getAllPods = '../api/kibana_csv_importer_plugin/es/pods';
+const createPodUrl = '../api/kibana_csv_importer_plugin/es/pod';
+const importUrl = '../api/kibana_csv_importer_plugin/es/import';
+
 uiRoutes.enable();
 uiRoutes
-.when('/', {
-  template,
-  resolve: {
-    currentTime($http) {
-      return $http.get('../api/kibana_csv_importer_plugin/example').then(function (resp) {
-        return resp.data.time;
-      });
+    .when('/', {
+        template,
+        resolve: {
+            currentTime($http) {
+                return $http.get('../api/kibana_csv_importer_plugin/example').then(function (resp) {
+                    return resp.data.time;
+                });
+            }
+        }
+    });
+
+const app = uiModules.get('app/kibana_csv_importer_plugin', [
+    'ngFileUpload'
+]);
+
+app.controller('FileUploadCtrl', ['$scope', '$injector', ($scope, $injector) => {
+    const defaultIdField = '_id';
+
+    const $timeout = $injector.get('$timeout');
+    const $window = $injector.get('$window');
+    const $http = $injector.get('$http');
+    const Upload = $injector.get('Upload');
+
+    // vm = view model
+    $scope.vm = {
+        idField: defaultIdField,
+        allPods: []
+    };
+
+    let vm = $scope.vm;
+
+    const onError = (error) => {
+        $window.alert(JSON.stringify(error, null, 2));
+    };
+
+    $http.get(getAllPods).then((response) => {
+        if(response.data.error) {
+            throw new Error(response.data.error);
+        }
+
+        vm.allPods = response.data.body.pods;
+    }).catch(onError);
+
+    vm.createNewPod = () => {
+        $http.post(createPodUrl).then((response) => {
+            if(response.data.error) {
+                throw new Error(response.data.error);
+            }
+
+            const newPodName = response.data.body.podName;
+
+            vm.podName = newPodName;
+            vm.allPods.unshift(newPodName);
+        }).catch(onError);
+    };
+
+    vm.onFilesDrop = (files) => {
+        vm.file = files[0];
+    };
+
+    vm.removeFile = () => {
+        vm.idField = defaultIdField;
+        delete vm.file;
+    };
+
+    vm.onSubmit = () => {
+        let fileName = _.get(vm.file, 'name', '');
+        if(!fileName){
+            return;
+        }
+
+        fileName = fileName.split('.csv')[0].toLowerCase();
+
+        vm.importInProgress = true;
+        vm.showImportSuccessMessage = false;
+        vm.errors = [];
+
+        let counter = 0;
+        Papa.parse(vm.file, {
+            header: true,
+            dynamicTyping: true,
+            step: (row) => {
+                const data = _.get(row, 'data[0]');
+                return $http.post(importUrl, {
+                    podName: vm.podName,
+                    type: fileName,
+                    data: _.extend(data, {
+                        id: ++counter
+                    })
+                }).then((response) => {
+                    if(response.data.error) {
+                        throw response.data.error;
+                    }
+                }).catch((error) => {
+                    vm.errors.push(JSON.stringify(error, null, 2));
+                });
+            },
+            complete: (results, file) => {
+                console.log('Parsing complete:', results, file);
+                $timeout(() => {
+                    vm.showImportSuccessMessage = vm.errors.length < 1;
+                    vm.importInProgress = false;
+                }, 1000);
+            },
+            error: (error) => {
+                vm.errors.push(JSON.stringify(error, null, 2));
+            },
+            skipEmptyLines: true
+        });
     }
-  }
-});
-
-uiModules
-.get('app/kibana_csv_importer_plugin', [])
-.controller('kibanaCsvImporterPluginHelloWorld', function ($scope, $route, $interval) {
-  $scope.title = 'Kibana Csv Importer Plugin';
-  $scope.description = 'An awesome Kibana plugin';
-
-  var currentTime = moment($route.current.locals.currentTime);
-  $scope.currentTime = currentTime.format('HH:mm:ss');
-  var unsubscribe = $interval(function () {
-    $scope.currentTime = currentTime.add(1, 'second').format('HH:mm:ss');
-  }, 1000);
-  $scope.$watch('$destroy', unsubscribe);
-});
+}]);
