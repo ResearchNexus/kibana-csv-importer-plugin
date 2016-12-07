d56c6e812b7697dee0a77403007b0db3ad173cd5
Miha Eržen <miha.erzen1@gmail.com>
2016-12-07 07:38:20 +0100

    Make it look nicer 💄

--- public/app.js
+++ public/app.js
@@ -2,8 +2,6 @@ import 'ng-file-upload';
 
 import _ from 'lodash';
 import Papa from 'papaparse';
-import moment from 'moment';
-import chrome from 'ui/chrome';
 import uiModules from 'ui/modules';
 import uiRoutes from 'ui/routes';
 
@@ -15,116 +13,124 @@ const getAllPods = '../api/kibana_csv_importer_plugin/es/pods';
 const createPodUrl = '../api/kibana_csv_importer_plugin/es/pod';
 const importUrl = '../api/kibana_csv_importer_plugin/es/import';
 
+const onError = (error) => {
+  window.alert(JSON.stringify(error, null, 2));
+};
+
 uiRoutes.enable();
 uiRoutes
-    .when('/', {
-        template,
-        resolve: {
-            currentTime($http) {
-                return $http.get('../api/kibana_csv_importer_plugin/example').then(function (resp) {
-                    return resp.data.time;
-                });
-            }
-        }
-    });
+  .when('/', {
+    template,
+    resolve: {
+      currentTime($http) {
+        return $http.get('../api/kibana_csv_importer_plugin/example').then(function (resp) {
+          return resp.data.time;
+        });
+      }
+    }
+  });
 
 const app = uiModules.get('app/kibana_csv_importer_plugin', [
-    'ngFileUpload'
+  'ngFileUpload'
 ]);
 
 app.controller('FileUploadCtrl', ['$scope', '$injector', ($scope, $injector) => {
-    const defaultIdField = '_id';
+  const $timeout = $injector.get('$timeout');
+  const $http = $injector.get('$http');
+  const Upload = $injector.get('Upload');
 
-    const $timeout = $injector.get('$timeout');
-    const $window = $injector.get('$window');
-    const $http = $injector.get('$http');
-    const Upload = $injector.get('Upload');
+  // vm = view model
+  $scope.vm = {
+    allPods: []
+  };
 
-    // vm = view model
-    $scope.vm = {
-        idField: defaultIdField,
-        allPods: []
-    };
+  let vm = $scope.vm;
 
-    let vm = $scope.vm;
-
-    const onError = (error) => {
-        $window.alert(JSON.stringify(error, null, 2));
-    };
-
-    $http.get(getAllPods).then((response) => {
-        if(response.data.error) {
-            throw new Error(response.data.error);
-        }
-
-        vm.allPods = response.data.body.pods;
-    }).catch(onError);
-
-    vm.createNewPod = () => {
-        $http.post(createPodUrl).then((response) => {
-            if(response.data.error) {
-                throw new Error(response.data.error);
-            }
-
-            const newPodName = response.data.body.podName;
-
-            vm.podName = newPodName;
-            vm.allPods.unshift(newPodName);
-        }).catch(onError);
-    };
-
-    vm.onFilesDrop = (files) => {
-        vm.file = files[0];
-    };
-
-    vm.removeFile = () => {
-        vm.idField = defaultIdField;
-        delete vm.file;
-    };
-
-    vm.onSubmit = () => {
-        let fileName = _.get(vm.file, 'name', '');
-        if(!fileName){
-            return;
-        }
-
-        fileName = fileName.split('.csv')[0].toLowerCase();
-
-        vm.importInProgress = true;
-        vm.showImportSuccessMessage = false;
-        vm.errors = [];
-
-        let counter = 0;
-        Papa.parse(vm.file, {
-            header: true,
-            dynamicTyping: true,
-            step: (row) => {
-                const data = _.get(row, 'data[0]');
-                return $http.post(importUrl, {
-                    podName: vm.podName,
-                    type: fileName,
-                    data: _.extend(data, {
-                        id: ++counter
-                    })
-                }).then((response) => {
-                    if(response.data.error) {
-                        throw response.data.error;
-                    }
-                }).catch((error) => {
-                    vm.errors.push(JSON.stringify(error, null, 2));
-                });
-            },
-            complete: (results, file) => {
-                console.log('Parsing complete:', results, file);
-                $timeout(() => {
-                    vm.showImportSuccessMessage = vm.errors.length < 1;
-                    vm.importInProgress = false;
-                }, 1000);
-            },
-            error: (error) => {
-                vm.errors.push(JSON.stringify(error, null, 2));
-            },
-            skipEmptyLines: true
-        });
+  // fetch all pods
+  $http.get(getAllPods).then((response) => {
+    if (response.data.error) {
+      throw new Error(response.data.error);
     }
+
+    vm.allPods = response.data.body.pods;
+  }).catch(onError);
+
+  const resetVmState = () => {
+    delete vm.showImportSuccessMessage;
+  };
+
+  vm.createNewPod = () => {
+    resetVmState();
+    vm.podCreationInProgress = true;
+    $http.post(createPodUrl)
+      .then((response) => {
+        if (response.data.error) {
+          throw new Error(response.data.error);
+        }
+
+        const newPodName = response.data.body.podName;
+
+        vm.podName = newPodName;
+        vm.allPods.unshift(newPodName);
+      })
+      .catch(onError)
+      .finally(() => {
+        vm.podCreationInProgress = false;
+      });
+  };
+
+  vm.onFilesDrop = (files) => {
+    vm.file = files[0];
+  };
+
+  vm.removeFile = () => {
+    resetVmState();
+    delete vm.file;
+  };
+
+  vm.onSubmit = () => {
+    let fileName = _.get(vm.file, 'name', '');
+    if (!fileName) {
+      return;
+    }
+
+    fileName = fileName.split('.csv')[0].toLowerCase();
+
+    vm.importInProgress = true;
+    vm.showImportSuccessMessage = false;
+    vm.errors = [];
+
+    let counter = 0;
+    Papa.parse(vm.file, {
+      header: true,
+      dynamicTyping: true,
+      step: (row) => {
+        const data = _.get(row, 'data[0]');
+        return $http.post(importUrl, {
+          podName: vm.podName,
+          type: fileName,
+          data: _.extend(data, {
+            id: ++counter
+          })
+        }).then((response) => {
+          if (response.data.error) {
+            throw response.data.error;
+          }
+        }).catch((error) => {
+          vm.errors.push(JSON.stringify(error, null, 2));
+        });
+      },
+      complete: (results, file) => {
+        console.log('Parsing complete:', results, file);
+        $timeout(() => {
+          vm.showImportSuccessMessage = vm.errors.length < 1;
+          vm.importInProgress = false;
+        }, 1000);
+      },
+      error: (error) => {
+        vm.errors.push(JSON.stringify(error, null, 2));
+      },
+      skipEmptyLines: true
+    });
+  };
 }]);
--- server/routes/es.js
+++ server/routes/es.js
@@ -1,110 +1,108 @@
-'use strict';
-
 import Promise from 'bluebird';
 import randomstring from 'randomstring';
 import {
-    name as pluginName
+  name as pluginName
 } from '../../package.json';
 
 const async = Promise.coroutine;
 const rootUrl = `/api/${pluginName}/es`;
 
 export default function (server) {
-    const client = server.plugins.elasticsearch.client;
+  const client = server.plugins.elasticsearch.client;
 
-    const generateRandomPodName = () => `pod_${randomstring.generate({capitalization:'lowercase'})}`;
+  const generateRandomPodName = () => `pod_${randomstring.generate({capitalization: 'lowercase'})}`;
 
-    const indexExists = function*(podName) {
-        try {
-            yield* client.indices.exists(podName);
-        } catch (e) {
-            return false;
-        }
+  const indexExists = function*(podName) {
+    try {
+      yield* client.indices.exists(podName);
+    } catch (e) {
+      return false;
+    }
 
-        return true;
-    };
+    return true;
+  };
 
-    const createRandomPod = async(function*() {
-        let podName = generateRandomPodName();
-        let exists = yield* indexExists(podName);
+  const createRandomPod = async(function*() {
+    let podName = generateRandomPodName();
+    let exists = yield* indexExists(podName);
 
-        while(exists) {
-            podName = generateRandomPodName();
-            exists = yield* indexExists(podName);
-        }
+    while (exists) {
+      podName = generateRandomPodName();
+      exists = yield* indexExists(podName);
+    }
 
-        yield client.indices.create({
-            index: podName
+    yield client.indices.create({
+      index: podName
+    });
+
+    return podName;
+  });
+
+  server.route({
+    path: `${rootUrl}/pods`,
+    method: 'GET',
+    handler: async(function*(req, reply) {
+      try {
+
+        const pods = (yield client.cat.indices({
+          h: 'index'
+        })).split('\n').filter((podName) => podName);
+
+        reply({
+          body: {
+            pods
+          }
+        });
+      } catch (error) {
+        reply({
+          error
+        });
+      }
+    })
+  });
+
+  server.route({
+    path: `${rootUrl}/pod`,
+    method: 'POST',
+    handler: async(function*(req, reply) {
+      try {
+        let podName = yield createRandomPod();
+
+        reply({
+          body: {
+            podName
+          }
+        });
+      } catch (error) {
+        reply({
+          error
+        });
+      }
+    })
+  });
+
+  server.route({
+    path: `${rootUrl}/import`,
+    method: 'POST',
+    handler: async(function*(req, reply) {
+      const payload = req.payload;
+
+      try {
+        const response = yield client.create({
+          index: payload.podName,
+          type: payload.type,
+          id: payload.data.id,
+          body: payload.data
         });
 
-        return podName;
-    });
-
-    server.route({
-        path: `${rootUrl}/pods`,
-        method: 'GET',
-        handler: async(function*(req, reply) {
-            try {
-
-                const pods = (yield client.cat.indices({
-                    h: 'index'
-                })).split('\n').filter((podName) => podName);
-
-                reply({
-                    body: {
-                        pods
-                    }
-                });
-            } catch (error) {
-                reply({
-                    error
-                });
-            }
-        })
-    });
-
-    server.route({
-        path: `${rootUrl}/pod`,
-        method: 'POST',
-        handler: async(function*(req, reply) {
-            try {
-                let podName = yield createRandomPod();
-
-                reply({
-                    body: {
-                        podName
-                    }
-                });
-            } catch (error) {
-                reply({
-                    error
-                });
-            }
-        })
-    });
-
-    server.route({
-        path: `${rootUrl}/import`,
-        method: 'POST',
-        handler: async(function*(req, reply) {
-            const payload = req.payload;
-
-            try {
-                const response = yield client.create({
-                    index: payload.podName,
-                    type: payload.type,
-                    id: payload.data.id,
-                    body: payload.data
-                });
-
-                reply({
-                    body: response
-                });
-            } catch (error) {
-                reply({
-                    error
-                });
-            }
-        })
-    });
+        reply({
+          body: response
+        });
+      } catch (error) {
+        reply({
+          error
+        });
+      }
+    })
+  });
 };
